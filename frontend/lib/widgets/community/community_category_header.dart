88349de5e71b6d4ded338b3e8c74afbc0c2b872a
TM-view <thethirdman04@gmail.com>
2026-08-08 03:01:42 +0700

    separate part in community_select_page to widget

---
+++ frontend/lib/widgets/community/community_category_header.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class CommunityCategoryHeader extends StatelessWidget {
+  const CommunityCategoryHeader({
+    super.key,
+    required this.imageUrl,
+  });
+
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      pinned: true,
+      expandedHeight: 250,
+      backgroundColor: const Color.fromARGB(255, 245, 244, 244),
+      iconTheme: const IconThemeData(
+        color: Colors.white,
+      ),
+      flexibleSpace: LayoutBuilder(
+        builder: (context, constraints) {
+          return Stack(
+            fit: StackFit.expand,
+            children: [
+              Image.network(
+                imageUrl,
+                fit: BoxFit.cover,
+                alignment: Alignment.bottomCenter,
+              ),
+              Container(
+                color: Colors.black.withOpacity(0.2),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
